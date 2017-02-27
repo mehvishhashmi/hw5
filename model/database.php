11bbdac46e57a194a3e6e982cad7573efd2e8c17
MehvishHashmi <mh449@njit.edu>
2017-02-27 14:15:33 -0500

    changing loacl host

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
-    $username = 'mgs_user';
-    $password = 'pa55word';
+    $dsn = 'mysql:host=sql1.njit.edu;dbname=mh449';
+    $username = 'mh449';
+    $password = 'P8a5oOaq';
 
     try {
         $db = new PDO($dsn, $username, $password);
