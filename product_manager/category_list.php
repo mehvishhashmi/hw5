345224712f69fbe90c4a76434caafa9ec59a9dc6
mehvishhashmi <mh449@njit.edu>
2017-03-04 11:53:10 -0500

    category_list.php

--- product_manager/category_list.php
+++ product_manager/category_list.php
@@ -8,12 +8,29 @@
             <th>&nbsp;</th>
         </tr>        
         <!-- add category rows here -->
-    </table>
+	<?php foreach ($categories as $category) : ?>
+	   <tr>
+	   <td><?php echo $category['categoryName']; ?></td>
+	   <td><form action="delete_category.php" method="post">
+	   <input type="hidden" name="category_id"  value="<?php
+	   echo $category['categoryID']; ?>">
+	   <input type="submit" value="Delete">
+	   </form></td>
+	   </tr>
+	 <?php endforeach; ?>
+  
+</table>
 
     <h2>Add Category</h2>
     <!-- add code for form here -->
-
-    <p><a href="index.php?action=list_products">List Products</a></p>
+	<form action="add_category.php" method="post" id="add_category_form">
+	<label>Name:</label>
+	<input type="text" name="name"><br><br>
+	<label>&nbsp;</label>
+	<input type="submit" value="Add Category"><br>
+	</form>
+  
+  <p><a href="index.php?action=list_products">List Products</a></p>
 
 </main>
 <?php include '../view/footer.php'; ?>
